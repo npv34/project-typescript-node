585d1d3cab525e7762c2724dafe8bd2effafae68
npv34 <phanvanluan611996@gmail.com>
2023-03-27 15:30:09 +0700

    get  user login

--- src/routers/api.router.ts
+++ src/routers/api.router.ts
@@ -2,6 +2,7 @@ import express from "express";
 import {LoginController} from "../controllers/api/login.controller";
 import verifyJWT from "../middleware/jwt.middleware";
 import UserController from "../controllers/api/user.controller";
+import AuthController from "../controllers/api/auth.controller";
 
 
 const router = express.Router();
@@ -16,6 +17,10 @@ router.get('/users', verifyJWT, (req, res) => {
 
 router.delete('/users/:id', verifyJWT, (req, res) => {
     UserController.deleteUser(req, res)
+})
+
+router.get('/user-login', verifyJWT, (req: any, res: any)=>{
+    AuthController.me(req, res);
 })
 
 export default router;
