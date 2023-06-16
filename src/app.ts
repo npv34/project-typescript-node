6c676510a99b213b359edfa703b3d1e447acadcf
npv34 <phanvanluan611996@gmail.com>
2023-06-16 09:26:52 +0700

    demo ajax

--- src/app.ts
+++ src/app.ts
@@ -22,6 +22,7 @@ const port = 8080; // default port to listen
 
 app.set( "views", path.join( __dirname, "views" ) );
 app.set( "view engine", "ejs" );
+app.use(express.static( path.join( __dirname, "public")))
 
 app.use(bodyParser.urlencoded({ extended: true}))
 app.use(bodyParser.json());
